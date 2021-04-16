984fe71a42731868611be8c25055c9ec4ef586c8
Peter Benjamin Mhina <peterbenjaminmhina@Peters-MacBook-Pro.local>
2021-04-16 15:42:37 -0700

    New background

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {ThemeProvider} from "styled-components";
+import theme from "./theme"
 
-import { worker } from "./mocks/browser";
-worker.start();
+ ReactDOM.render(
+   <ThemeProvider theme={theme}>
+  <App />
+ </ThemeProvider>,
+    document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// render(
+//     <ThemeProvider theme={theme}>
+//         <App />
+//     </ThemeProvider>, 
+//     document.getElementById("root"));
+
+
+// import { worker } from "./mocks/browser";
+// worker.start();
+
+// ReactDOM.render(<App />, document.getElementById("root"));
